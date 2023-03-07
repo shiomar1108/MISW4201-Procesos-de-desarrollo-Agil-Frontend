000e42c3cbefb945e0cf09d8f4e3cce59f5625ea
Shiomar Salazar <s.salazarc@uniandes.edu.co>
2023-03-07 11:49:14 -0600

    (Azul) Se agregan validaciones al campo de Contraseña y Confirmar Contraseña

--- src/app/usuario/usuario-registro/usuario-registro.component.ts
+++ src/app/usuario/usuario-registro/usuario-registro.component.ts
@@ -7,10 +7,9 @@ import { UsuarioService } from '../usuario.service';
 @Component({
   selector: 'app-usuario-registro',
   templateUrl: './usuario-registro.component.html',
-  styleUrls: ['./usuario-registro.component.css']
+  styleUrls: ['./usuario-registro.component.css'],
 })
 export class UsuarioRegistroComponent implements OnInit {
-
   usuarioForm: FormGroup;
 
   constructor(
@@ -19,29 +18,79 @@ export class UsuarioRegistroComponent implements OnInit {
     private router: Router,
     private toastrService: ToastrService
   ) {
-    this.usuarioForm = new FormGroup('')
+    this.usuarioForm = new FormGroup('');
   }
 
   ngOnInit() {
     this.usuarioForm = this.formBuilder.group({
-      nombre: ["", [Validators.required, Validators.maxLength(50), Validators.minLength(4)]],
-      apellido: ["", [Validators.required, Validators.maxLength(50), Validators.minLength(4)]],
-      usuario: ["", [Validators.required, Validators.maxLength(50), Validators.minLength(4)]],
-      password: ["", [Validators.required, Validators.maxLength(50), Validators.minLength(4)]],
-      confirmPassword: ["", [Validators.required, Validators.maxLength(50), Validators.minLength(4)]]
+      nombre: [
+        '',
+        [
+          Validators.required,
+          Validators.maxLength(50),
+          Validators.minLength(4),
+        ],
+      ],
+      apellido: [
+        '',
+        [
+          Validators.required,
+          Validators.maxLength(50),
+          Validators.minLength(4),
+        ],
+      ],
+      usuario: [
+        '',
+        [
+          Validators.required,
+          Validators.maxLength(50),
+          Validators.minLength(4),
+        ],
+      ],
+      password: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(8),
+          Validators.maxLength(50),
+          Validators.pattern(
+            '(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[A-Za-z0-9].{8,}'
+          ),
+        ],
+      ],
+      confirmPassword: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(8),
+          Validators.maxLength(50),
+          Validators.pattern(
+            '(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[A-Za-z0-9].{8,}'
+          ),
+        ],
+      ],
     });
   }
 
   registrarUsuario() {
-    this.usuarioService.registro(this.usuarioForm.get('nombre')?.value,
-      this.usuarioForm.get('apellido')?.value,
-      this.usuarioForm.get('usuario')?.value,
-      this.usuarioForm.get('password')?.value)
-      .subscribe(res => {
-        this.router.navigate([`/`])
-      },
-        error => {
-          this.toastrService.error("Error en el registro. Verifique que el usuario no se encuentre ya registrado", "Error", { closeButton: true });
-        })
+    this.usuarioService
+      .registro(
+        this.usuarioForm.get('nombre')?.value,
+        this.usuarioForm.get('apellido')?.value,
+        this.usuarioForm.get('usuario')?.value,
+        this.usuarioForm.get('password')?.value
+      )
+      .subscribe(
+        (res) => {
+          this.router.navigate([`/`]);
+        },
+        (error) => {
+          this.toastrService.error(
+            'Error en el registro. Verifique que el usuario no se encuentre ya registrado',
+            'Error',
+            { closeButton: true }
+          );
+        }
+      );
   }
 }
